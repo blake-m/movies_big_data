6763b3e55736ce85c5e82039549c1db3ba053e6a
blake-m <blazej.marciniak.dent@gmail.com>
2020-01-29 10:03:16 +0100

    Create a working version of Q1 Python

--- bash/run_locally_most_popular_rating_above_4_mr.sh
+++ bash/run_locally_most_popular_rating_above_4_mr.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PYTHONSCRIPT="/home/blake/PycharmProjects/BigDataMovies/python/most_popular_rating_above_4_mr.py"
-DATACSV="/home/blake/PycharmProjects/BigDataMovies/the-movies-dataset/ratings.csv"
+RATINGSCSV="/home/blake/PycharmProjects/BigDataMovies/the-movies-dataset/ratings.csv"
+MOVIESCSV="/home/blake/PycharmProjects/BigDataMovies/the-movies-dataset/movies_metadata.csv"
 
-python  $PYTHONSCRIPT -r local $DATACSV
+python  $PYTHONSCRIPT -r local $RATINGSCSV $MOVIESCSV
